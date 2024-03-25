d3d88120aa71292ee4f7c0cbaa0fc714f721583c
Mohamed Gamal <gmgm60ayahoo.com@gmail.com>
2024-03-25 22:51:36 +0200

    add app banners on home screen

---
+++ lib/generated/models/app_banner.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of '../../models/app_banner.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$AppBannerImpl _$$AppBannerImplFromJson(Map<String, dynamic> json) =>
+    _$AppBannerImpl(
+      image: json['image'] as String,
+      url: json['url'] as String,
+    );
+
+Map<String, dynamic> _$$AppBannerImplToJson(_$AppBannerImpl instance) =>
+    <String, dynamic>{
+      'image': instance.image,
+      'url': instance.url,
+    };
